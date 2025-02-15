d7ee52046ff2359dcee4ce6292b3e49b8b34669e
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-15 14:55:11 -0800

    initialize server with Express, add environment configuration, and set up Vercel deployment

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running');
+});
+
+// app.use('/api', apiRouter);
+
+export default app;
