48c674a2548efaa546cc534888f5c7b14938d928
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-15 15:40:53 -0800

    refactor cookie handling in API routes for clarity and consistency

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors(
     {
         origin: process.env.CORS_ORIGIN,
-        credentials: true,
+        credentials: true
     }
 ));
 app.use(helmet());
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,19 @@ import express from 'express';
 const apiRouter = express.Router();
 
 apiRouter.get('/set-cookie', (_req, res) => {
-  res.cookie('name', 'express').json('cookie set');
+  // Set a cookie with the name 'name' and the value 'express'
+  res.cookie('name', 'express');
+
+  // Send a response to the client
+  res.json('cookie set');
 });
 
 apiRouter.get('/delete-cookie', (_req, res) => {
-  res.clearCookie('name').json('cookie deleted');
+  // Clear the cookie with the name 'name'
+  res.clearCookie('name');
+
+  // Send a response to the client
+  res.json('cookie deleted');
 });
 
 export default apiRouter;
