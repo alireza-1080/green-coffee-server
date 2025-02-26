6aa421bea9644e5c44cd9355ef0ee98953e0a68c
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-26 02:13:22 -0800

    uncomment root endpoint to enable server status response

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// app.get('/', (_req, res) => {
-//   res.json('Server is up and running');
-// });
+app.get('/', (_req, res) => {
+  res.json('Server is up and running');
+});
 
 app.use('/api', apiRouter);
 
