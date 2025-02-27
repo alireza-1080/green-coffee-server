2f2367af209114c5eaa416923e9fcad6174a2da9
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-27 01:22:55 -0800

    update mongoose connection options and add tr46 dependency

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,10 @@ const mongoUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri, {});
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed');
